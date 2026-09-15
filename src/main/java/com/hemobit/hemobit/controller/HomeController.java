7c3dd0087970394ec403726915f66deb30ccc4c4
brenocrruz <breno.cruz.dev@outlook.com>
2026-09-15 11:28:07 -0300

    fix: corrige pacote do HomeController

--- src/main/java/com/hemobit/hemobit/controller/HomeController.java
+++ src/main/java/com/hemobit/hemobit/controller/HomeController.java
@@ -1,4 +1,4 @@
-package br.com.cesar.hemobit.controller;
+package com.hemobit.hemobit.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
